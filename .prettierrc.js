0a6d69c881446d14eef30b59fb9b5e1b130a374f
xbai <1411804691@qq.com>
2024-10-11 20:14:53 +0800

    项目初始化

---
+++ .prettierrc.js
@@ -0,0 +1,34 @@
+/*
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2024-10-11 19:44:53
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2024-10-11 19:54:32
+ * @FilePath: \react-ts-test\.prettierrc.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+//.prettier.js
+export default {
+  // 箭头函数只有一个参数的时候可以忽略括号
+  arrowParens: 'avoid',
+  // 括号内部不要出现空格
+  bracketSpacing: true,
+  // 行结束符使用 Unix 格式
+  endOfLine: 'lf',
+  // true: Put > on the last line instead of at a new line
+  jsxBracketSameLine: false,
+  // 行宽
+  printWidth: 100,
+  // 换行方式
+  proseWrap: 'preserve',
+  // 分号
+  semi: true,
+  // 使用单引号
+  singleQuote: true,
+  // 缩进
+  tabWidth: 2,
+  // 使用 tab 缩进
+  useTabs: false,
+  // 后置逗号，多行对象、数组在最后一行增加逗号
+  trailingComma: 'es5',
+  parser: 'typescript',
+};
